82e28114c36dcd1e943f5e2c2879503b79ddcbbc
Dino Herlambang <dino.herlambang@gmail.com>
2024-10-21 13:14:35 +0700

    add option for windows startup option

--- dh_usb_controller.py
+++ dh_usb_controller.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import winreg
 from PyQt5 import QtWidgets, QtGui
 
@@ -16,11 +17,18 @@ class USBControllerApp(QtWidgets.QSystemTrayIcon):
         # Add options to enable, disable USB, and exit
         enable_action = self.menu.addAction("Enable USB")
         disable_action = self.menu.addAction("Disable USB")
+        
+        # Add startup toggle option
+        self.startup_action = self.menu.addAction("Run at Startup")
+        self.startup_action.setCheckable(True)
+        self.startup_action.setChecked(self.is_startup_enabled())
+        
         exit_action = self.menu.addAction("Exit")
 
         # Connect the actions to methods
         enable_action.triggered.connect(self.enable_usb)
         disable_action.triggered.connect(self.disable_usb)
+        self.startup_action.triggered.connect(self.toggle_startup)
         exit_action.triggered.connect(self.exit_app)
 
         # Set the menu to the system tray icon
@@ -54,8 +62,38 @@ class USBControllerApp(QtWidgets.QSystemTrayIcon):
     def disable_usb(self):
         self.modify_usb_registry(enable=False)
 
+    def is_startup_enabled(self):
+        key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
+        try:
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path, 0, winreg.KEY_READ)
+            winreg.QueryValueEx(key, "DH USB Controller")
+            winreg.CloseKey(key)
+            return True
+        except WindowsError:
+            return False
+
+    def toggle_startup(self):
+        success = toggle_startup(self.startup_action.isChecked())
+        if not success:
+            self.startup_action.setChecked(not self.startup_action.isChecked())
+            QtWidgets.QMessageBox.warning(None, "Startup Error", "Failed to modify startup settings.")
+
     def exit_app(self):
         QtWidgets.QApplication.quit()
+
+def toggle_startup(enable):
+    key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path, 0, winreg.KEY_ALL_ACCESS)
+        if enable:
+            executable_path = sys.executable if getattr(sys, 'frozen', False) else __file__
+            winreg.SetValueEx(key, "DH USB Controller", 0, winreg.REG_SZ, executable_path)
+        else:
+            winreg.DeleteValue(key, "DH USB Controller")
+        winreg.CloseKey(key)
+        return True
+    except WindowsError:
+        return False
 
 if __name__ == '__main__':
     # Ensure the app has admin privileges (optional)
